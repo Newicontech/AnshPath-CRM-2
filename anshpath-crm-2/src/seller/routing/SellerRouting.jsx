f3f4d418b430e4570b4fbf7b3050c915fdf33e95
Akash7501 <akashchavan7501@gmail.com>
2025-06-21 19:54:15 +0530

    [Akash]-spareParttaskTask4

--- anshpath-crm-2/src/seller/routing/SellerRouting.jsx
+++ anshpath-crm-2/src/seller/routing/SellerRouting.jsx
@@ -4,7 +4,7 @@ import DashboardOverview from '../components/dashboard/DashboardOverview';
 import SparePartAdd from '../components/inventry/SparePartAdd';
 import LessStockTable from '../components/inventry/LessStockTable';
 import ProductList from '../components/products/ProductList';
-
+import SparePartsInventory from '../components/inventry/sparePartInventry/dummyinvetry/SparePartsInventory'
 
 const SellerRouting = () => (
   <Routes>
@@ -13,11 +13,11 @@ const SellerRouting = () => (
     <Route path="/dashboard/overview" element={<DashboardOverview />} />
     <Route path="/inventory/sparePartsAdd" element={<SparePartAdd />} />
     <Route path="/inventory/lessStockTable" element={<LessStockTable />} />
-    <Route path="/addProducts" element={<AddProducts />} />
+    {/* <Route path="/addProducts" element={<AddProducts />} /> */}
 
-    <Route path="/products/productList" element={<ProductList />} />
+    <Route path="/AddSparePart" element={< SparePartsInventory/>} />
 
-  </Routes>
+ </Routes>
 );
 
 export default SellerRouting;
