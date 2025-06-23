5bbfc4815500be59a86636839708b27203922495
Newicontech <shweta.gurav1423@gmail.com>
2025-06-23 16:44:19 +0530

    Ketan Commite

--- anshpath-crm-2/src/seller/routing/SellerRouting.jsx
+++ anshpath-crm-2/src/seller/routing/SellerRouting.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import DashboardOverview from '../components/dashboard/DashboardOverview';
 import SparePartAdd from '../components/inventry/SparePartAdd';
 import LessStockTable from '../components/inventry/LessStockTable';
-import ProductList from '../components/products/ProductList';
 
 
 const SellerRouting = () => (
@@ -13,9 +12,6 @@ const SellerRouting = () => (
     <Route path="/dashboard/overview" element={<DashboardOverview />} />
     <Route path="/inventory/sparePartsAdd" element={<SparePartAdd />} />
     <Route path="/inventory/lessStockTable" element={<LessStockTable />} />
-    <Route path="/addProducts" element={<AddProducts />} />
-
-    <Route path="/products/productList" element={<ProductList />} />
 
   </Routes>
 );
